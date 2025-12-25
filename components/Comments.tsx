71c6e92103438c09fffc6ae44c0cf7f771e7c6f8
epicbus <122236362+epicbus@users.noreply.github.com>
2025-12-25 19:54:31 +0700

    Added Commenting Component
    Updated to use Giscus to manage discussions

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -10,17 +10,18 @@ export default function Comments() {
 
     const script = document.createElement("script");
     script.src = "https://giscus.app/client.js";
-    script.setAttribute("data-repo", "YOUR_GITHUB_USERNAME/bradjames-blog");
-    script.setAttribute("data-repo-id", "YOUR_REPO_ID");
-    script.setAttribute("data-category", "Comments");
-    script.setAttribute("data-category-id", "YOUR_CATEGORY_ID");
+    script.setAttribute("data-repo", "epicbus/bradjames-blog");
+    script.setAttribute("data-repo-id", "R_kgDOQuswQQ");
+    script.setAttribute("data-category", "Announcements");
+    script.setAttribute("data-category-id", "DIC_kwDOQuswQc4C0Ozn");
     script.setAttribute("data-mapping", "pathname");
     script.setAttribute("data-strict", "0");
     script.setAttribute("data-reactions-enabled", "1");
-    script.setAttribute("data-emit-metadata", "0");
-    script.setAttribute("data-input-position", "bottom");
+    script.setAttribute("data-emit-metadata", "1");
+    script.setAttribute("data-input-position", "top");
     script.setAttribute("data-theme", "preferred_color_scheme");
     script.setAttribute("data-lang", "en");
+    script.setAttribute("data-loading", "lazy");
     script.crossOrigin = "anonymous";
     script.async = true;
 
